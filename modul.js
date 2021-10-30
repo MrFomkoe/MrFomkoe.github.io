235f75091053f03c97dc80c66e8aa65bac8669a2
DevCosts <lenerts@inbox.lv>
2021-10-30 12:23:49 +0300

    added login scoreboard modules

--- modul.js
+++ modul.js
@@ -1,26 +1,49 @@
 
 // Get the modal
  var modal = document.getElementById("myModal");
- 
+ var modall = document.getElementById("myLogin");
+ var modalll = document.getElementById("myScoreboard");
+
  // Get the button that opens the modal
  var btn = document.getElementById("myBtn");
- 
+ var btnn = document.getElementById("login")
+ var btnnn = document.getElementById("scoreboard")
+
  // Get the <span> element that closes the modal
  var span = document.getElementsByClassName("close")[0];
- 
+ var spann = document.getElementsByClassName("close-login")[0];
+ var spannn = document.getElementsByClassName("close-scoreboard")[0];
+
  // When the user clicks the button, open the modal 
  btn.onclick = function() {
    modal.style.display = "block";
  }
  
+ btnn.onclick = function () {
+  modall.style.display = "block"
+ }
+
+ btnnn.onclick = function () {
+  modalll.style.display = "block" 
+  }
+
+
  // When the user clicks on <span> (x), close the modal
  span.onclick = function() {
-   modal.style.display = "none";
- }
+  modal.style.display = "none";
+}
+
+spann.onclick = function() {
+  modall.style.display = "none"
+}
+
+spannn.onclick = function() {
+  modalll.style.display = "none"
+}
  
  // When the user clicks anywhere outside of the modal, close it
- window.onclick = function(event) {
-   if (event.target == modal) {
-     modal.style.display = "none";
-   }
- }
+//  window.onclick = function(event) {
+//    if (event.target == modal) {
+//      modal.style.display = "none";
+//    }
+//  }
