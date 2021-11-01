d0835ea1f2fee4e8a89cc23b18f53bd400d0674d
Roman Ponomarjov <romanponomarjov@gmail.com>
2021-11-01 22:35:48 +0200

    made a complete merge with main branch 01/11/21

---
+++ audio.js
@@ -0,0 +1,11 @@
+const audio = document.getElementById('audio');
+audio.volume = 0.5;
+
+function audioPlay(){
+    audio.play();
+}
+
+function changeThemeToTavern(){
+    audio.src = './media/audio/tavern-theme.mp3';
+    audio.play();
+}
--- index.js
+++ index.js
@@ -1,28 +1,106 @@
-// const btnAboutUs = document.getElementById("about-us");
-// const btnLogin = document.getElementById("login");
-// const btnScoreboard = document.getElementById("scoreboard");
-
-
-function clickHandler() {
-    let name = document.getElementById("name").value;
-    confirm(`Hi, ${name}! Are you ready for some game?`);
-  }
+const btnAboutUs = document.getElementById("about-us");
+const btnLogin = document.getElementById("login");
+const btnScoreboard = document.getElementById("scoreboard");
+const headingContent = document.querySelectorAll('.heading-content');
+const playerNameSpan = document.querySelectorAll('.playerNameSpan');
+const logoContainer = document.querySelector('.logoContainer');
+const logo = document.querySelector('.logo');
+const logoAll = document.querySelectorAll('.logoAll');
+const navbar = document.querySelector('.navbar');
+const startBTN = document.querySelector('.startBTN')
 
 
 // Array (massive) with pictures of characters
 const characters = document.querySelectorAll(".js-character");
-console.log(characters);
 
-// starting point
-var activeCharacter = 0;
+// character change arrows
+const arrowLeft = document.getElementById("arrowLeft");
+const arrowRight = document.getElementById("arrowRight");
 
-// function for changing character pic to next
-function next(){
+// starting point for character change
+let activeCharacter = 0;
 
-    // removes "active" class from the current "character"
-    characters[activeCharacter].classList.remove("active");
-    // changes character to next in array
-        ++activeCharacter;
+
+// function to place logo, shownavbar and play music
+logoAll.forEach(function (element){
+    element.addEventListener("click", logoChange)
+});
+
+// logoAll.forEach(function (element){
+//     element.removeEventListener("click", logoChange)
+// });
+
+function logoChange(){
+        console.log(logoContainer);
+        logoAll.forEach(function (element){
+            element.removeEventListener("click", logoChange)
+        });
+        logoContainer.animate([
+            {opacity: '0'}
+        ],  {
+            duration: 2000,
+            iterations: 1,
+            fill: 'forwards'
+        });   
+        logo.animate([
+            {transform: 'scale(0.65, 0.65) rotate(360deg)'},
+        ], {
+            duration: 1500,
+            easing: 'ease-out',
+            iterations: 1,
+            fill: 'forwards'
+        });
+        navbar.animate([
+            {opacity: '1'}
+        ],  {
+            duration: 5000,
+            iterations: 1,
+            fill: 'forwards'
+        });   
+
+        startBTN.animate([
+            {opacity: '1'}
+        ],  {
+            duration: 5000,
+            iterations: 1,
+            fill: 'forwards'
+        });     
+        
+        audio.animate([
+            {opacity: '1'}
+        ],  {
+            duration: 5000,
+            iterations: 1,
+            fill: 'forwards'
+        });
+        
+        setTimeout(function(){
+            logoContainer.style.display = 'none';
+        }, 1000);
+
+        audioPlay()
+};
+
+
+// function that adds name of the player to all headings where there is address to the player
+function addName (){
+    let playerName = document.getElementById("name").value;
+    console.log(playerName)
+    if (playerName == ''){
+        playerName = 'Player';
+    }
+    headingContent.forEach(function (heading){
+        heading.innerHTML = playerName;
+    });
+    playerNameSpan.forEach(function (heading){
+        heading.innerHTML = playerName + ', ';
+    });
+}
+
+// changing character to next
+arrowRight.addEventListener("click", function () {
+      characters[activeCharacter].classList.remove("active");
+      ++activeCharacter;
     
         // check - if the current character is the last in array - active character changes to first
         if (activeCharacter >= characters.length) {
@@ -31,10 +109,11 @@ function next(){
 
     // add "active" class to current character
     characters[activeCharacter].classList.add("active");
-    console.log(activeCharacter);
-}
+    // console.log('active character is: ', activeCharacter);
+});
 
-function prev(){
+// changing character to previous
+arrowLeft.addEventListener("click", function () {
     characters[activeCharacter].classList.remove("active");
     --activeCharacter;
 
@@ -43,7 +122,34 @@ function prev(){
         }
      
     characters[activeCharacter].classList.add("active");
-    console.log(activeCharacter);
+    // console.log('active character is: ', activeCharacter);
+});
+
+// function that adds the character 
+function addCharacter(){
+    let wrapperContent = document.querySelector('.slide-wrapper-content');
+    let characterToShow = document.createElement('IMG');
+    characterToShow.src = characters[activeCharacter].src;
+    characterToShow.className = 'character active';
+
+    characterToShow.style.cssText = `
+        position: relative;
+        width: 250px;
+        height: 250px;
+        opacity: 0;
+        `;
+    document.querySelector('.activeCharacter').appendChild(characterToShow);
+    increaseOpacityCharacter(characterToShow);
+    increaseOpacityCharacter(wrapperContent);
 }
 
+// function that changes opacity for the added character
+function increaseOpacityCharacter(characterToShow){
+    characterToShow.animate([
+        {opacity: 1,},
+    ],  {
+        duration: 2000,
+        iterations: 1,
+        fill: 'forwards'})
+}
 
